02cd910ee4ccb39bbb8851fb9f26b1967afa352e
nbiancaniello <nbiancaniello@gmail.com>
2016-10-13 16:47:29 -0300

    Mapas

--- FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MainActivity.java
+++ FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
-
-    public final static String ALTITUD = "0";
-    public final static String LATITUD = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void enviarCoordenadas(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
-        EditText caltitud = (EditText) findViewById(R.id.caja_altitud);
-        EditText clatitud = (EditText) findViewById(R.id.caja_latitud);
-        intent.putExtra(ALTITUD, caltitud.getText().toString());
-        intent.putExtra(LATITUD, clatitud.getText().toString());
-//        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        EditText latitud = (EditText) findViewById(R.id.box_latitude);
+        EditText longitud = (EditText) findViewById(R.id.box_longitude);
+        Bundle extras = new Bundle();
+
+        extras.putString("LATITUDE", latitud.getText().toString());
+        extras.putString("LONGITUDE", longitud.getText().toString());
+        intent.putExtras(extras);
         intent.setClassName(this,"com.nico.mapa.MapsActivity");
         startActivity(intent);
+
+//        Coordenadas de CABA
+//        Latitud: 34.604° Sur
+//        Longitud: 58.382° Oeste
+
     }
 }
--- FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MapsActivity.java
+++ FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MapsActivity.java
@@ -3,6 +3,7 @@ package com.nico.mapa;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,27 +26,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         Intent intent = getIntent();
-        String altitud = intent.getStringExtra(MainActivity.ALTITUD);
-        String latitud = intent.getStringExtra(MainActivity.LATITUD);
+        Bundle extras = intent.getExtras();
 
         // Add a marker in Sydney and move the camera
-        LatLng buenosAires = new LatLng(Integer.valueOf(altitud), Integer.valueOf(latitud));
-        mMap.addMarker(new MarkerOptions().position(buenosAires).title("Marker in Buenos Aires"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(buenosAires));
+        LatLng coordenates = new LatLng(Double.valueOf(extras.getString("LATITUDE")),Double.valueOf(extras.getString("LONGITUDE")));
+        mMap.addMarker(new MarkerOptions().position(coordenates).draggable(true).title("Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordenates));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
+
     }
 }
