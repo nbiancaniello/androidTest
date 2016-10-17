7257e0a7d5f478de6361fdedc2ade106b5903377
nbiancaniello <nbiancaniello@gmail.com>
2016-10-17 10:26:05 -0300

    mapas

--- FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MainActivity.java
+++ FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nico.mapa;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Parcelable;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,9 +12,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+import java.util.Locale;
+
 import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +27,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+//    @Override
+//    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+//        super.onRestoreInstanceState(savedInstanceState, persistentState);
+//        Intent intent = getIntent();
+//        Bundle extras = intent.getExtras();
+//        Toast.makeText(getApplicationContext(),extras.getString("LONGITUDE"),Toast.LENGTH_SHORT).show();
+//    }
+
     public void enviarCoordenadas(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
-        EditText latitud = (EditText) findViewById(R.id.box_latitude);
-        EditText longitud = (EditText) findViewById(R.id.box_longitude);
+        EditText latitude = (EditText) findViewById(R.id.box_latitude);
+        EditText longitude = (EditText) findViewById(R.id.box_longitude);
         Bundle extras = new Bundle();
 
-        extras.putString("LATITUDE", latitud.getText().toString());
-        extras.putString("LONGITUDE", longitud.getText().toString());
+        extras.putString("LATITUDE", latitude.getText().toString());
+        extras.putString("LONGITUDE", longitude.getText().toString());
         intent.putExtras(extras);
         intent.setClassName(this,"com.nico.mapa.MapsActivity");
-        startActivity(intent);
+        startActivityForResult(intent,REQUEST_CODE);
 
 //        Coordenadas de CABA
 //        Latitud: 34.604° Sur
 //        Longitud: 58.382° Oeste
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Log.d("LATITUDE", data.getStringExtra("LATITUDE"));
+                Log.d("LONGITUDE", data.getStringExtra("LONGITUDE"));
+
+                try {
+                    Geocoder geo = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addresses = geo.getFromLocation(Double.valueOf(data.getStringExtra("LATITUDE")),Double.valueOf(data.getStringExtra("LONGITUDE")), 1);
+                    if (addresses.isEmpty()) {
+                        Log.d("ADDRESS", "Waiting for Location");
+                    }
+                    else {
+                        if (addresses.size() > 0) {
+                            Log.d("ADDRESS", addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+                                    addresses.get(0).getLocality() +", " +  // Localidad
+                                    addresses.get(0).getAdminArea() + ", " +  // Municipìo
+                                    addresses.get(0).getCountryName()); // País
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "No hay información para mostrar", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "No hay información para mostrar", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MapsActivity.java
+++ FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MapsActivity.java
@@ -4,17 +4,23 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
     private GoogleMap mMap;
+    private LatLng position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button demo = (Button) this.findViewById(R.id.demo);
+        demo.setOnClickListener(devolverCoordenadas);
     }
+
+    View.OnClickListener devolverCoordenadas = new View.OnClickListener() {
+        public void onClick(View v) {
+            Log.i("Posicion", String.format("Drag final %f:%f",
+                    position.latitude,
+                    position.longitude));
+
+            Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+            Bundle extras = new Bundle();
+
+            extras.putString("LATITUDE", String.valueOf(position.latitude));
+            extras.putString("LONGITUDE", String.valueOf(position.longitude));
+            intent.putExtras(extras);
+            intent.setClassName(getApplicationContext(),"com.nico.mapa.MainActivity");
+//            startActivity(intent);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -39,5 +67,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(coordenates));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
 
+        mMap.setOnMarkerDragListener(this);
+    }
+
+
+    public void onMarkerDragStart(Marker marker) {
+        position=marker.getPosition();
+
+        Log.d(getClass().getSimpleName(), String.format("Drag from %f:%f",
+                position.latitude,
+                position.longitude));
+    }
+
+    public void onMarkerDrag(Marker marker) {
+        position=marker.getPosition();
+
+        Log.d(getClass().getSimpleName(),
+                String.format("Dragging to %f:%f", position.latitude,
+                        position.longitude));
+    }
+
+    public void onMarkerDragEnd(Marker marker) {
+        position=marker.getPosition();
+
+        Log.d(getClass().getSimpleName(), String.format("Dragged to %f:%f",
+                position.latitude,
+                position.longitude));
     }
 }
