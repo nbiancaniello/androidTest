e031f4b2be937efe72d8ed3d6642be178009afbe
nbiancaniello <nbiancaniello@gmail.com>
2016-10-17 15:44:45 -0300

    mapas

--- FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MainActivity.java
+++ FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MainActivity.java
@@ -3,15 +3,15 @@ package com.nico.mapa;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
-import android.os.Parcelable;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 //        Toast.makeText(getApplicationContext(),extras.getString("LONGITUDE"),Toast.LENGTH_SHORT).show();
 //    }
 
-    public void enviarCoordenadas(View view) {
+    public void checkCoordinates(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         EditText latitude = (EditText) findViewById(R.id.box_latitude);
         EditText longitude = (EditText) findViewById(R.id.box_longitude);
@@ -53,6 +53,62 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void getAddress(View view){
+
+        EditText latitude = (EditText) findViewById(R.id.box_latitude);
+        EditText longitude = (EditText) findViewById(R.id.box_longitude);
+        try {
+            Geocoder geo = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geo.getFromLocation(Double.valueOf(String.valueOf(latitude.getText())),Double.valueOf(String.valueOf(longitude.getText())), 1);
+            if (addresses.isEmpty()) {
+                Log.d("ADDRESS", "Waiting for Location");
+            } else {
+                if (addresses.size() > 0) {
+//                            Log.d("ADDRESS", addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+//                                    addresses.get(0).getLocality() +", " +  // Localidad
+//                                    addresses.get(0).getAdminArea() + ", " +  // Municipìo
+//                                    addresses.get(0).getCountryName()); // País
+
+                    TextView text = (TextView) findViewById(R.id.text_addressshown);
+                    text.setText(addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+                            addresses.get(0).getLocality() +", " +  // Localidad
+                            addresses.get(0).getAdminArea() + ", " +  // Municipìo
+                            addresses.get(0).getCountryName()); // País
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public void getCurrentAddress(View view){
+//
+//        EditText latitude = (EditText) findViewById(R.id.box_latitude);
+//        EditText longitude = (EditText) findViewById(R.id.box_longitude);
+//        try {
+//            Geocoder geo = new Geocoder(getApplicationContext(), Locale.getDefault());
+//            List<Address> addresses = geo.getFromLocation(Double.valueOf(String.valueOf(latitude.getText())),Double.valueOf(String.valueOf(longitude.getText())), 1);
+//            if (addresses.isEmpty()) {
+//                Log.d("ADDRESS", "Waiting for Location");
+//            } else {
+//                if (addresses.size() > 0) {
+////                            Log.d("ADDRESS", addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+////                                    addresses.get(0).getLocality() +", " +  // Localidad
+////                                    addresses.get(0).getAdminArea() + ", " +  // Municipìo
+////                                    addresses.get(0).getCountryName()); // País
+//
+//                    TextView text = (TextView) findViewById(R.id.text_addressshown);
+//                    text.setText(addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+//                            addresses.get(0).getLocality() +", " +  // Localidad
+//                            addresses.get(0).getAdminArea() + ", " +  // Municipìo
+//                            addresses.get(0).getCountryName()); // País
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE) {
@@ -65,10 +121,15 @@ public class MainActivity extends AppCompatActivity {
                     List<Address> addresses = geo.getFromLocation(Double.valueOf(data.getStringExtra("LATITUDE")),Double.valueOf(data.getStringExtra("LONGITUDE")), 1);
                     if (addresses.isEmpty()) {
                         Log.d("ADDRESS", "Waiting for Location");
-                    }
-                    else {
+                    } else {
                         if (addresses.size() > 0) {
-                            Log.d("ADDRESS", addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+//                            Log.d("ADDRESS", addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
+//                                    addresses.get(0).getLocality() +", " +  // Localidad
+//                                    addresses.get(0).getAdminArea() + ", " +  // Municipìo
+//                                    addresses.get(0).getCountryName()); // País
+
+                            TextView text = (TextView) findViewById(R.id.text_addressshown);
+                            text.setText(addresses.get(0).getAddressLine(0) + ", " + // Dirección Completa
                                     addresses.get(0).getLocality() +", " +  // Localidad
                                     addresses.get(0).getAdminArea() + ", " +  // Municipìo
                                     addresses.get(0).getCountryName()); // País
--- FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MapsActivity.java
+++ FirebaseCRUD-master/Mapa/app/src/main/java/com/nico/mapa/MapsActivity.java
@@ -3,11 +3,8 @@ package com.nico.mapa;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,15 +28,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        Button demo = (Button) this.findViewById(R.id.demo);
+        Button demo = (Button) this.findViewById(R.id.button_showaddress);
         demo.setOnClickListener(devolverCoordenadas);
     }
 
     View.OnClickListener devolverCoordenadas = new View.OnClickListener() {
         public void onClick(View v) {
-            Log.i("Posicion", String.format("Drag final %f:%f",
-                    position.latitude,
-                    position.longitude));
 
             Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
             Bundle extras = new Bundle();
@@ -47,8 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             extras.putString("LATITUDE", String.valueOf(position.latitude));
             extras.putString("LONGITUDE", String.valueOf(position.longitude));
             intent.putExtras(extras);
-            intent.setClassName(getApplicationContext(),"com.nico.mapa.MainActivity");
-//            startActivity(intent);
+            intent.setClassName(getApplicationContext(), "com.nico.mapa.MainActivity");
             setResult(RESULT_OK, intent);
             finish();
         }
@@ -57,41 +50,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
-        // Add a marker in Sydney and move the camera
         LatLng coordenates = new LatLng(Double.valueOf(extras.getString("LATITUDE")),Double.valueOf(extras.getString("LONGITUDE")));
         mMap.addMarker(new MarkerOptions().position(coordenates).draggable(true).title("Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(coordenates));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
 
         mMap.setOnMarkerDragListener(this);
+        position = coordenates;
+
     }
 
 
     public void onMarkerDragStart(Marker marker) {
-        position=marker.getPosition();
+//        position=marker.getPosition();
 
-        Log.d(getClass().getSimpleName(), String.format("Drag from %f:%f",
-                position.latitude,
-                position.longitude));
+//        Log.d(getClass().getSimpleName(), String.format("Drag from %f:%f",
+//                position.latitude,
+//                position.longitude));
     }
 
     public void onMarkerDrag(Marker marker) {
-        position=marker.getPosition();
+//        position=marker.getPosition();
 
-        Log.d(getClass().getSimpleName(),
-                String.format("Dragging to %f:%f", position.latitude,
-                        position.longitude));
+//        Log.d(getClass().getSimpleName(),
+//                String.format("Dragging to %f:%f", position.latitude,
+//                        position.longitude));
     }
 
     public void onMarkerDragEnd(Marker marker) {
         position=marker.getPosition();
 
-        Log.d(getClass().getSimpleName(), String.format("Dragged to %f:%f",
-                position.latitude,
-                position.longitude));
+//        Log.d(getClass().getSimpleName(), String.format("Dragged to %f:%f",
+//                position.latitude,
+//                position.longitude));
     }
 }
